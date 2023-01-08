2b8566f3dafce165bc6e4450b2f149bf4c6d7426
Caio Souza <souzsdev@gmail.com>
2023-01-08 14:58:21 -0300

    Correct url api axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:3000";
+const url = "https://next-dash-souzzs.vercel.app";
 
 const axiosInstance = axios.create({
   baseURL: `${url}/api`,
